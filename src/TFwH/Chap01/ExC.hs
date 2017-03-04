b1950f238fe837ae6edb4dbf7458a84b8669e141
Nobuo Yamashita <nobsun@sampou.org>
2017-03-04 15:18:18 +0900

    modify haddock format

--- src/TFwH/Chap01/ExC.hs
+++ src/TFwH/Chap01/ExC.hs
@@ -1,25 +1,22 @@
-{- |
-   練習問題 C
--}
+-- |
+-- 練習問題 C
+--
+-- >>> :type 'H'
+-- 'H' :: Char
+-- >>> :type "H"
+-- "H" :: [Char]
+-- >>> :type 2001
+-- 2001 :: Num t => t
+-- >>> :type "2001"
+-- "2001" :: [Char]
+-- >>> [1, 2, 3] ++ [3, 2, 1]
+-- [1,2,3,3,2,1]
+-- >>> "Hello" ++ " World!"
+-- "Hello World!"
+-- >>> [1, 2, 3] ++ []
+-- [1,2,3]
+-- >>> "Hello" ++ " " ++ "World!"
+-- "Hello World!"
+
 module TFwH.Chap01.ExC where
 
-{- 
-
->>> :type 'H'
-'H' :: Char
->>> :type "H"
-"H" :: [Char]
->>> :type 2001
-2001 :: Num t => t
->>> :type "2001"
-"2001" :: [Char]
->>> [1, 2, 3] ++ [3, 2, 1]
-[1,2,3,3,2,1]
->>> "Hello" ++ " World!"
-"Hello World!"
->>> [1, 2, 3] ++ []
-[1,2,3]
->>> "Hello" ++ " " ++ "World!"
-"Hello World!"
-
--}
