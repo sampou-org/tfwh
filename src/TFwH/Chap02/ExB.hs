7e7148b2b93e8bcf8290a1ee025687c6337a5ebd
Nobuo Yamashita <nobsun@sampou.org>
2017-03-29 04:57:21 +0900

    add src/TFwH/Chap02/ExB.hs

---
+++ src/TFwH/Chap02/ExB.hs
@@ -0,0 +1,28 @@
+-- |
+-- 第2章 練習問題 B
+--
+-- GHCi のプロンプトで以下のコマンドを順に試してみよう．
+--
+-- @
+-- :type [0, 1)
+-- :type double -3
+-- :type double (-3)
+-- :type double double 0
+-- :type if 1 == 0 then 2 == 1
+-- :type "++" == "+" ++ "+"
+-- :type [(+), (-)]
+-- :type [[], [[]], [[[]]]]
+-- :type concat ["tea", "for", '2']
+-- :type concat ["tea", "for", "2"]
+-- @
+--
+module TFwH.Chap02.ExB where
+
+-- |
+--
+-- @
+-- double = (2 *)
+-- @
+--
+double :: Int -> Int
+double = (2 *)
