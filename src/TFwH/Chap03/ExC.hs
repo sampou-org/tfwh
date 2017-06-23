f07de0858cf92f43511be60fff1940c6ac02e66e
Nobuo Yamashita <nobsun@sampou.org>
2017-06-23 13:57:22 +0900

    add Exercise C in Chapter 3

---
+++ src/TFwH/Chap03/ExC.hs
@@ -0,0 +1,23 @@
+-- |
+-- = 第3章 練習問題 C
+--
+-- * @div@ を以下のように定義できるか
+--
+-- @
+-- div :: Integral a => a -> a -> a
+-- div x y = floor ( x / y)
+-- @
+--
+-- 答： (/) は Integral クラスでは定義されていないのでできない．
+--
+module TFwH.Chap03.ExC where
+
+import Prelude hiding (div)
+
+-- |
+--
+-- >>> div 7 4
+-- 1
+--
+div :: Integral a => a -> a -> a
+div x y = floor (fromIntegral x / fromIntegral y)
