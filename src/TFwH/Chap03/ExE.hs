2c282dff4f3adf56082d7aa9188d592ddba8a287
Nobuo Yamashita <nobsun@sampou.org>
2017-06-25 10:46:31 +0900

    modified

--- src/TFwH/Chap03/ExE.hs
+++ src/TFwH/Chap03/ExE.hs
@@ -30,24 +30,35 @@ type Interval = (Integer, Integer)
 --
 -- >>> isqrt 123456.78
 -- 351
-
+--
 isqrt :: Float -> Integer
 isqrt x = fst (until unit (shrink x) (bound x))
   where
     unit (m,n) = m + 1 == n
-    
+
+-- |
+-- 区間の縮小
+--
 shrink :: Float -> Interval -> Interval
 shrink x (m, n) = if (p * p) `leq` x then (p, n) else (m, p)
   where
     p = (m + n) `div` 2
 
+-- |
+-- 最初の区間
 bound :: Float -> Interval
 bound x = (0, until above (* 2) 1)
   where
     above n = x `lt` (n * n)
 
+-- |
+-- ≦
+--
 leq :: Integer -> Float -> Bool
 m `leq` x = fromIntegral m <= x
 
+--|
+-- ＜
+--
 lt :: Float -> Integer -> Bool
 x `lt` n = x < fromIntegral n
